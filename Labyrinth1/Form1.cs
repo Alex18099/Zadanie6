fac5cc2689a9cf716129d3a9820a4ad4402debf5
Alex18099 <sasharodionow@gmail.com>
2023-06-20 17:35:29 +0300

    Добавьте файлы проекта.

---
+++ Labyrinth1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labyrinth1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(this.Location.X + 55, this.Location.Y + 60);
+        }
+
+        private void Form1_load(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(this.Location.X + 55, this.Location.Y + 60);
+        }
+
+        private void labelFinish_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Victory!");
+        }
+    }
+}
